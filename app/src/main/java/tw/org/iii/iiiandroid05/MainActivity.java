0ff64313d345fa795a8668ff5b15de167ffcc02e
DCH <dching0121@gmail.com>
2019-11-12 10:51:09 +0800

    ListView練習

---
+++ app/src/main/java/tw/org/iii/iiiandroid05/MainActivity.java
@@ -0,0 +1,55 @@
+package tw.org.iii.iiiandroid05;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView listView;
+    private SimpleAdapter adapter;
+    //ArrayList適合不常變動的資料，LinkedList適合常常異動的資料
+    private LinkedList<HashMap<String,String>> data; //元素是HashMap，Key的資料型態是String，Value也是String
+    private String[] from = {"title", "mesg"};
+    private int[] to = {R.id.item_title, R.id.item_mesg};
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        listView = findViewById(R.id.listview);
+        initListView();
+    }
+
+    private void initListView() {
+        data = new LinkedList<>();
+
+        for (int i=0; i<30; i++) {
+            HashMap<String,String> dd = new HashMap<>();
+            int rand = (int)(Math.random()*49+1);
+            dd.put(from[0], "Title" + rand);
+            dd.put(from[1], "Content" + rand);
+            data.add(dd);
+        }
+        adapter = new SimpleAdapter(
+                this, data,
+                R.layout.item_test1,
+                from, to);
+        listView.setAdapter(adapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.v("DCH", "pos"+position);
+            }
+        });
+    }
+}
